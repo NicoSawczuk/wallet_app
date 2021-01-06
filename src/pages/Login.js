3350e5558671e7bcd05639ebc917be5428101339
Nicolas Sawczuk <nico.290698@hotmail.com>
2021-01-06 20:01:43 -0300

    Update Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,15 @@
-import React, {useEffect} from 'react'
-import { Form, Input, Button, Alert } from 'antd';
+import React, { useEffect } from 'react'
+import { Form, Input, Button, Card } from 'antd';
 import { message } from 'antd'
 import useUser from './../hooks/useUser';
 
 
 export default function Login() {
-
+    const gridStyle = {
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    };
     const { login, error, errorMessage } = useUser();
 
     const layout = {
@@ -21,56 +25,60 @@ export default function Login() {
     };
 
     useEffect(function () {
-        if (error){
+        if (error) {
             message.error(errorMessage)
         }
     }, [error])
 
     return (
-        <div className="container mt-1">
-            <div className="row text-center">
-                <div className="col-md-12 m-t-md">
-                    <Form
-                        {...layout}
-                        name="basic"
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
+        <div className="container">
+            <div className="m-t-md">
+                <Card
+                    title="Login"
+                    style={gridStyle}
+                    bordered={false}
+                    size="small">
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name={'email'}
+                        label="Email"
+                        rules={[
+                            {
+                                type: 'email',
+                            },
+                            {
+                                required: true,
+                                message: 'Please input your email!'
+                            }
+                        ]}
                     >
-                        <Form.Item
-                            name={'email'}
-                            label="Email"
-                            rules={[
-                                {
-                                    type: 'email',
-                                },
-                                {
-                                    required: true,
-                                    message: 'Please input your email!'
-                                }
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
+                        <Input />
+                    </Form.Item>
 
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
 
-                        {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                    {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
                             <Checkbox>Remember me</Checkbox>
                         </Form.Item> */}
 
-                        <Form.Item {...tailLayout}>
-                            <Button type="primary" htmlType="submit">
-                                Submit
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
                             </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
+                    </Form.Item>
+                </Form>
+                </Card>
             </div>
         </div>
     );
